b4a0deb4680cc95a5f8973731d62562831f84712
GGlin <guangze@iappsasia.com>
2022-09-28 19:03:44 +0800

    Add storage and and the ability to associate an image with the notes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "NoteImage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
